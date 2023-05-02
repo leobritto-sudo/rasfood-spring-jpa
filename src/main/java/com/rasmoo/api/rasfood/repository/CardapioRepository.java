0dc0e2aca7af03eda451055607fe794ae6c6af50
Leonardo Alves <littocraft@gmail.com>
2023-05-02 10:13:52 -0300

    inserção endereço controller e repository

---
+++ src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
@@ -0,0 +1,7 @@
+package com.rasmoo.api.rasfood.repository;
+
+import com.rasmoo.api.rasfood.entity.Cardapio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardapioRepository extends JpaRepository<Cardapio, Integer> {
+}
